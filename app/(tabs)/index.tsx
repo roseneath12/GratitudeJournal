11dca3863e95dba10b06ea29c17440b8db2e2816
Inma  Ruiz <inma@fitovitalia.com>
2026-02-15 11:56:12 +0000

    changes to new app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Today was good because ...</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
